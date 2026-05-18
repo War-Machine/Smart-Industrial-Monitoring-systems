f9604337f93e43a6df8a21b0c474fc85c2acdb39
Ank47# <105866972+War-Machine@users.noreply.github.com>
2026-05-19 00:41:06 +0530

    Added Arduino source code

---
+++ code/main.ino
@@ -0,0 +1,11 @@
+C++
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("Smart Industrial Monitoring System");
+}
+
+void loop() {
+  Serial.println("Monitoring Industrial Parameters...");
+  delay(2000);
+}
