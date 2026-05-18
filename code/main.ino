b98565495d8c1890fbd4d489343de4d93c06c670
Ank47# <105866972+War-Machine@users.noreply.github.com>
2026-05-19 00:51:19 +0530

    added sensor monitoring and alert system

--- code/main.ino
+++ code/main.ino
@@ -1,11 +1,47 @@
-C++
+#include <DHT.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+#define GAS_SENSOR A0
+#define BUZZER 8
+
+DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(9600);
-  Serial.println("Smart Industrial Monitoring System");
+  pinMode(BUZZER, OUTPUT);
+
+  dht.begin();
+
+  Serial.println("Smart Industrial Monitoring System Started");
 }
 
 void loop() {
-  Serial.println("Monitoring Industrial Parameters...");
+
+  float temperature = dht.readTemperature();
+  float humidity = dht.readHumidity();
+
+  int gasValue = analogRead(GAS_SENSOR);
+
+  Serial.print("Temperature: ");
+  Serial.print(temperature);
+  Serial.println(" °C");
+
+  Serial.print("Humidity: ");
+  Serial.print(humidity);
+  Serial.println(" %");
+
+  Serial.print("Gas Sensor Value: ");
+  Serial.println(gasValue);
+
+  if (gasValue > 400) {
+    digitalWrite(BUZZER, HIGH);
+    Serial.println("Warning! Gas Leakage Detected!");
+  } else {
+    digitalWrite(BUZZER, LOW);
+  }
+
+  Serial.println("-------------------------");
+
   delay(2000);
 }
